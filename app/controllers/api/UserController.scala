f109d73b3e4767e0e5a69d0ccd5ec4ed35ec014c
gerhynes <gkhynes@gmail.com>
2022-05-25 13:06:23 +0100

    Add create, update and delete functionality

--- app/controllers/api/UserController.scala
+++ app/controllers/api/UserController.scala
@@ -1,15 +1,15 @@
 package controllers.api
 
-import models.User
+import dao.UserDao
+import models.{User, UserForm}
 import play.api.libs.json.Json
 import play.api.mvc._
-import dao.UserDao
 
 import javax.inject._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class UserController @Inject()(val controllerComponents: ControllerComponents, val userDao: UserDao)(implicit ec: ExecutionContext) extends BaseController{
+class UserController @Inject()(val controllerComponents: ControllerComponents, val userDao: UserDao)(implicit ec: ExecutionContext) extends BaseController {
 
   implicit val userFormat = Json.format[User]
 
@@ -20,18 +20,51 @@ class UserController @Inject()(val controllerComponents: ControllerComponents, v
 
   def findById(id: String) = Action.async {
     val userFuture = userDao.findById(id.toInt)
-    userFuture.map(user => Ok(Json.toJson(user)))
+    userFuture.map(user => {
+      println(user)
+      Ok(Json.toJson(user))
+    }
+    )
   }
 
-  def create = Action { implicit request =>
-    Created("New user created")
+  def create = Action.async { implicit request =>
+    UserForm.form.bindFromRequest.fold(
+      errorForm => {
+        errorForm.errors.foreach(println)
+        Future.successful(BadRequest("Something went wrong!"))
+      },
+      data => {
+        val user = User(0, data.name, data.isAdmin)
+        val userFuture = userDao.create(user)
+        userFuture.map(userId => Created(s"User $userId created"))
+      }
+    )
   }
 
-  def update(id: String) = Action {
-    Ok(s"User $id updated")
+  def update(id: String) = Action.async { implicit request: Request[AnyContent] =>
+    UserForm.form.bindFromRequest.fold(
+      errorForm => {
+        errorForm.errors.foreach(println)
+        Future.successful(BadRequest("Something went wrong!"))
+      },
+      data => {
+        val user = User(id.toInt, data.name, data.isAdmin)
+        val userFuture = userDao.update(user)
+        userFuture.map(_ => Ok(s"User $id updated"))
+      }
+    )
   }
 
-  def delete(id: String) = Action {
-    Ok(s"User $id deleted")
+  def delete(id: String) = Action.async { implicit request: Request[AnyContent] =>
+    UserForm.form.bindFromRequest.fold(
+      errorForm => {
+        errorForm.errors.foreach(println)
+        Future.successful(BadRequest("Something went wrong!"))
+      },
+      data => {
+        val userFuture = userDao.delete(id.toInt)
+        userFuture.map(_ => Ok(s"User $id deleted"))
+      }
+    )
   }
 }
